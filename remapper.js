4284701ff1a5888fc02982e8718d8ec1bd7455cc
Dominic Tarr <dominic.tarr@gmail.com>
2011-01-01 15:51:25 +1300

    discovered some problems in remapper. doesn't save the dependencies right. I had hacked that up some complicated way before I wrote traverser. made some notes, about to rewrite.

--- remapper.js
+++ remapper.js
@@ -32,17 +32,32 @@ function Maker (depends,loaded,remaps){
   }
 
   self.load = function (id, filename, parent, makeR ){
+    /*
+      when a file is loaded,
+    */
 
     if(!loaded[id]) { loaded[id] = {} }
+      //if this is the first time the module is loaded, 
+      //it will not have a record yet.
+      
+      //wait untill defaultLoad returns, make will have created the record.
+
 
     if(loaded[parent.id]) { loaded[parent.id][id] = loaded[id] }
 
     return modules.defaultLoad(id, filename, parent, makeR)
+    //check that loaded[id] exists, and
+    //set loaded[parent.id][id] = loaded[id]
+
+    //why didn't I do it like this the first time?
   }
 
   self.make = function (thisModule){
     var id = thisModule.id
     depends[id] = loaded[id]
+    //when a file is first loaded,
+    //create an empty record in loaded under thisModule.id
+    //there should not already be a record there.
     
     return modules.makeMake(new Maker(loaded[id],loaded,remaps))(thisModule)  
   }
